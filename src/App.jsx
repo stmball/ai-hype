7eff25447a8c8ef6ef4af1d6d955659b7e11cfe9
Samuel Ball <stmball@gmail.com>
2026-06-03 19:08:20 +0100

    pages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import MailPage from './components/MailPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/mail" element={<MailPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+      </HashRouter>
   );
 }
 
